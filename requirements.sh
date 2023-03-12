a53adc3689e9046dc60e5a9fe0819ca2def5439a
Yoni Haziza <97190263+d4rkiZ@users.noreply.github.com>
2023-03-12 11:18:02 +0200

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo '[@] installing "jq" [@]'
+apt install jq -y
+
+echo '[@] installing "xterm" [@]'
+apt install xterm -y
+sleep 1.5
+
+echo '[@] installing "ngrok" [@]'
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+
+echo '[@] DONE, happy hacking[@]'
+
+
+
+
+
+
+
+
