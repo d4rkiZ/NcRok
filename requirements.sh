03519d9c258c6ca5e80da05726608d833c472419
Yoni Haziza <97190263+d4rkiZ@users.noreply.github.com>
2023-03-13 14:56:27 +0200

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -7,6 +7,14 @@ echo '[@] installing "xterm" [@]'
 apt install xterm -y
 sleep 1.5
 
+echo '[@] installing "konsole" [@]'
+apt install konsole -y
+sleep 1
+
+echo '[@] installing "gnome-terminal" [@]'
+apt install xterm -y
+sleep 1.5
+
 echo '[@] installing "ngrok" [@]'
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 
